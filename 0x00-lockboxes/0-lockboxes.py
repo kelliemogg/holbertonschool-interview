06b839a077e0aa5b740f967394be3b70372d8acc
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-22 19:04:44 +0000

    pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,29 +5,31 @@
 """ Add unique values up to the length of the test list of lists """
 """ compare the lengths of the test example and the newly made list """
 
-def join(T,R):
+
+def join(T, R):
     """ join the test and results list """
     res = []
     for e in R:
         res += T[e]
     return res
 
+
 def canUnlockAll(boxes):
     """ Unlock all indexes based on values present in a list of lists """
     index = 0
-    total = list(set(boxes[0])| {0})
+    total = list(set(boxes[0]) | {0})
     added = True
     if len(boxes) == len(boxes[0]):
         return True
     try:
         while added:
             added = False
-            for j in join(boxes,total[index:]):
+            for j in join(boxes, total[index:]):
                 if j not in total and len(total) < len(boxes):
                     total.append(j)
                     index += 1
                     added = True
     except IndexError:
         return False
-  
+
     return len(total) == len(boxes)

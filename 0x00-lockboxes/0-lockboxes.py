457f536e9ce193af40a58a9939dd8eb7f576cd4e
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-22 18:28:34 +0000

    added try accept to account for IndexError

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -17,12 +17,15 @@ def canUnlockAll(boxes):
     index = 0
     total = list(set(boxes[0])| {0})
     added = True
-    while added:
-        added = False
-        for j in join(boxes,total[index:]):
-            if j not in total:
-                total.append(j)
-                index += 1
-                added = True
+    try:
+        while added:
+            added = False
+            for j in join(boxes,total[index:]):
+                if j not in total and len(total) < len(boxes):
+                    total.append(j)
+                    index += 1
+                    added = True
+    except IndexError:
+        return False
   
     return len(total) == len(boxes)

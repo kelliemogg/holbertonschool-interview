c45919560e2e11831956b0175ae2a3ead40b4937
Kellie Mogg <kelliedmogg@gmail.com>
2021-08-04 19:17:06 +0000

    set left and right to null - no check

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -15,7 +15,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     newb = malloc(sizeof(binary_tree_t));
 
     if (!value)
-        free (newb);
         return NULL;
 
     /* setting value to the new node */

2df2489cef3f4c5c069548338ac2476470f2a614
Kellie Mogg <kelliedmogg@gmail.com>
2021-08-04 19:46:07 +0000

    unsure

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+* heap_insert - inserts a value into a Max Binary Heap
+* @root: dbl ptr to the root node of the heap
+* @value: value of the node to be inserted
+* Return: ptr to the inserted node
+*/
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+    binary_tree_t *newb;
+
+    newb = malloc(sizeof(heap_t));
+
+    /* null checks */
+    if (!newb || !value)
+        return (NULL);
+
+    /* adding value to newb node */
+    newb->n = value;
+
+    if (*root == NULL)
+        *root = newb;
+    return (newb);
+}

74639b6d55ed861dec8b8e548c8fa5d120aeda98
Kellie Mogg <kelliedmogg@gmail.com>
2021-08-04 19:07:38 +0000

    correct output

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/*
+* binary_tree_node - creates a node
+* description: creates a binary tree node
+* @parent: ptr to the parent node
+* @value: value for new node
+* return: ptr to new node or NULL for fail
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newb;
+    newb = malloc(sizeof(binary_tree_t));
+    
+    if (!value)
+        return NULL;
+
+    /* setting value to the new node */
+    newb->n = value;
+    /* setting *parent as parent to new node */
+    newb->parent = parent;
+    return (newb);
+}

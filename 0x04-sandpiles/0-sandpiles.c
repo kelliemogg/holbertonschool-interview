3df365ab4ccb0f94539aaa3e382140f29c9d13d4
kelliemogg <kelliedmogg@gmail.com>
2021-08-21 16:35:12 -0500

    betty

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -9,68 +9,73 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    /* indexes for each grid */
-    int a, b;
-    int stable;
+	/* indexes for each grid */
+	int a, b;
+	int stable;
 
-    for (a = 0; a < 3; a++)
-    {
-        for (b = 0; b < 3; b++)
+	for (a = 0; a < 3; a++)
+	{
+		for (b = 0; b < 3; b++)
         {
             grid1[a][b] = grid1[a][b] + grid2[a][b];
         }
-    }
-    stable = stable_grid(grid1);
-    if (stable == 1)
-    {
-        print_grid(grid1);
-    }
-    else
-    {
-        for (a = 0; a < 3; a++)
-        {
-            for (b = 0; b < 3; b++)
-            {
-                if (grid1[a][b] >= 4)
-                    grid1[a][b] -= 4;
-            }
-        }
-    }
-    
+	}
+	stable = stable_grid(grid1);
+	if (stable == 1)
+	{
+		print_grid(grid1);
+	}
+	else
+	{
+		for (a = 0; a < 3; a++)
+		{
+			for (b = 0; b < 3; b++)
+			{
+				if (grid1[a][b] >= 4)
+					grid1[a][b] -= 4;
+			}
+		}
+	}
 }
 
 /**
 * stable_grid - checks stability of new grid
-* @gridSum: sum of grid1 and grid2
+* @grid: sum of grid1 and grid2
 * Return: 1 or 0
 */
 
-int stable_grid(int gridSum[3][3])
+int stable_grid(int grid[3][3])
 {
-    int a, b;
-    for (a = 0; a < 3; a++)
-    {
-        for (b = 0; b < 3; b++)
-        {
-            if (gridSum[a][b] > 3)
-                return 1;
-        }
-    }
-    return 0;
+	int a, b;
+	for (a = 0; a < 3; a++)
+	{
+		for (b = 0; b < 3; b++)
+		{
+			if (grid[a][b] > 3)
+				return 1;
+		}
+	}
+	return 0;
 }
+
+/**
+* print_grid - prints a given 3x3 grid
+* @grid: sum of grid1 and grid2
+* Return: 1 or 0
+*/
 
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }

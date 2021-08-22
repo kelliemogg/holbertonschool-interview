bb5596064b0facaad2ea34d000b3992caa0eef4a
kelliemogg <kelliedmogg@gmail.com>
2021-08-22 14:32:56 -0500

    no topping

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -10,25 +10,59 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	/* indexes for each grid */
-	int a, b;
+	int i, j;
 	int stable;
 
-	for (a = 0; a < 3; a++)
+	for (i = 0; i < 3; i++)
 	{
-		for (b = 0; b < 3; b++)
+		for (j = 0; j < 3; j++)
 		{
-			grid1[a][b] = grid1[a][b] + grid2[a][b];
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
 		}
 	}
 	stable = stable_grid(grid1);
-	while (stable == 0)
+	while (stable == 1)
 	{
-		for (a = 0; a < 3; a++)
+		int top = 0, down = 0, left = 0, right = 0;
+		int processAgain = 0;
+		for (i = 0; i < 3; i++)
 		{
-			for (b = 0; b < 3; b++)
+			for (j = 0; j < 3; j++)
 			{
-				if (grid1[a][b] >= 4)
-					grid1[a][b] -= 4;
+				if (grid1[i][j] > 3)
+				{
+					if (i - 1 >= 0)
+					{
+						top = 1;
+						grid1[i - 1][j] += 1;
+						if (grid1[i - 1][j] >= 4)
+							processAgain = 1;
+					}
+					if (i + 1 < 3)
+					{
+						down = 1;
+						grid1[i + 1][j] += 1;
+						if (grid1[i + 1][j] >= 4)
+							processAgain = 1;
+					}
+					if (j - 1 >= 0)
+					{
+						left = 1;
+						grid1[i][j - 1] += 1;
+						if (grid1[i][j - 1] >= 4)
+							processAgain = 1;
+					}
+					if (j + 1 < 3)
+					{
+						right = 1;
+						grid1[i][j + 1] += 1;
+						if (grid1[i][j + 1] >= 4)
+							processAgain = 1;
+					}
+				grid1[i][j] -= (top + down + left + right);
+				if (grid1[i][j] >= 4)
+					processAgain = 1;
+				}
 			}
 		}
 		print_grid(grid1);
@@ -43,13 +77,13 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 
 int stable_grid(int grid[3][3])
 {
-	int a, b;
+	int i, j;
 
-	for (a = 0; a < 3; a++)
+	for (i = 0; i < 3; i++)
 	{
-		for (b = 0; b < 3; b++)
+		for (j = 0; j < 3; j++)
 		{
-			if (grid[a][b] > 3)
+			if (grid[i][j] > 3)
 				return (1);
 		}
 	}

e3667d31071013dc0ded6c60bd5e32221ee7d925
kelliemogg <kelliedmogg@gmail.com>
2021-08-25 17:52:07 -0500

    base files

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* is_palindrome - palindrome check
+* description: determines if a linked list is a palindrome
+* @head: pointer to the head of the list
+* Return: 0 or 1
+*/
+
+int is_palindrome(listint_t **head)
+{
+    return 0;
+}

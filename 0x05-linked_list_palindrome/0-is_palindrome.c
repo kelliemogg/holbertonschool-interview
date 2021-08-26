a726ef562b53d4f27c96b4243e19c4bd0659718b
kelliemogg <kelliedmogg@gmail.com>
2021-08-26 13:21:12 -0500

    Solved using recursion

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,13 +1,35 @@
 #include "lists.h"
 
 /**
-* is_palindrome - palindrome check
-* description: determines if a linked list is a palindrome
+* is_palindrome - palindrome check on a linked list
 * @head: pointer to the head of the list
 * Return: 0 or 1
 */
 
 int is_palindrome(listint_t **head)
 {
-    return 0;
+	if (!*head)
+		return (checkPalin(head, *head));
+		return (1);
+}
+
+/**
+* checkPalin - recursive function
+* description: to check if a linked list is palindrome
+* @head: pointer to the head of the list
+* Return: 0 or 1
+*/
+int checkPalin(listint_t **left, listint_t *right)
+{
+	/* base case */
+	if (!right)
+		return (1);
+
+	int result = checkPalin(left, right->next) && ((*left)->n == right->n);
+	if (result)
+	{
+		(*left) = (*left)->next;
+		return (1);
+	}
+	return (0);
 }

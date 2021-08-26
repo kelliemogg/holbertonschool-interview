4ff63689a1e4993dda26799d0ebc69788ccf198b
kelliemogg <kelliedmogg@gmail.com>
2021-08-26 14:18:02 -0500

    got rid of c++ comment style

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,43 +1,6 @@
 #include "lists.h"
 
 
-// /**
-// * is_palindrome - palindrome check on a linked list
-// * @head: pointer to the head of the list
-// * Return: 0 or 1
-// */
-
-// int is_palindrome(listint_t **head)
-// {
-// 	if (!*head)
-// 		return (checkPalin(head, *head));
-// 		return (1);
-// }
-
-// /**
-// * checkPalin - recursive function
-// * description: to check if a linked list is palindrome
-// * @head: pointer to the head of the list
-// * Return: 0 or 1
-// */
-// int checkPalin(listint_t **left, listint_t *right)
-// {
-// 	/* base case */
-// 	if (!right)
-// 		return (1);
-
-// 	int result = (checkPalin(left, right->next));
-// 	if (result == 0)
-// 		return (0);
-// 	if ((*left)->n == right->n)
-// 	{
-// 		*left = (*left)->next;
-// 		return (1);
-// 	}
-// 	else
-// 		return (0);
-// }
-
 /**
  * is_palindrome - checks if a single linked list is a palindrome
  * @head: points to the head of the linked list

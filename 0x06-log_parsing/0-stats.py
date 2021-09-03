72d657977019c17ac5292d4366906853412c034d
Kellie Mogg <kelliedmogg@gmail.com>
2021-09-03 03:01:07 +0000

    help

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ sorts through logs """
 import sys
+import fileinput
 from collections import defaultdict
 
 if __name__ == '__main__':
     f_size = 0
+    total_size = 0
     count = 0
-    status_codes = {
+    status_code = {
         '200': 0,
         '301': 0,
         '400': 0,
@@ -17,24 +19,25 @@ if __name__ == '__main__':
         '500': 0
     }
 
-    for line in sys.stdin.read():
-        args = line.split(" ")
-        count = count + 1
-        try:
+    try:
+        for line in fileinput.input():
+            args = line.split(" ")
+            count = count + 1
             if args != 8:
                 pass
-            else:
-                f_size += sys.argv[-1]
-                if code in status_code.keys():
-                    status[code] += 1
-                if (count % 10 == 0):
-                    # add file size from input to variable
-                    print("File size: {}".format(f_size))
-                    for code in sorted(status_code.items()):
-                        if code[1] != 0:
-                            print("{}: {}".format(code[0], code[1]))
-                    
-        except KeyboardInterrupt as err:
-            print("File size: {}".format(f_size))
-            for code in status_code.items:
-                print("{}: {}".format(code[0], code[1]))
+            if code in status_code.keys():
+                status[code] += 1
+            f_size = delim[len(delim) - 1]
+            if f_size.isdigit():
+                total_size += int(f_size)
+            if (count % 10 == 0):
+                # add file size from input to variable
+                print("File size: {}".format(f_size))
+                for code in sorted(status_code.items()):
+                    if code[1] != 0:
+                        print("{}: {}".format(code[0], code[1]))
+                        
+    except KeyboardInterrupt as err:
+        print("File size: {}".format(f_size))
+        for code in status_code.items:
+            print("{}: {}".format(code[0], code[1]))

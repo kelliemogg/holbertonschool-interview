72c7b3681ef21cd0c238330944bd265c1093d5d7
kelliemogg <kelliedmogg@gmail.com>
2021-09-08 19:12:32 -0800

    linked list - cycle detection

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check for a cycle in a linked list
+ * @list: the linked list
+ * Return: 0 or 1
+ **/
+
+int check_cycle(listint_t *list)
+{
+    listint_t *slow_p = list, *fast_p = list->next;
+ 
+    while (slow_p && fast_p && fast_p->next)
+    {
+        slow_p = slow_p->next;
+        fast_p = fast_p->next->next;
+        if (slow_p == fast_p) 
+        {
+            return 1;
+        }
+    }
+    return 0;
+}

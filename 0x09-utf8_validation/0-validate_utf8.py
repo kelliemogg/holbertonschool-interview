9f6e8aa36e5ca77643068d58495d71b1cfeacc9b
Kellie Mogg <kelliedmogg@gmail.com>
2021-09-24 02:31:39 +0000

    interview problem - empty

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Determines if a given data set represents a valid UTF-8 encoding """
+
+
+def validUTF8(data):
+    '''Method that returns true if data is valid UTF-8'''
+    valid_utf8 = True
+    if (data):
+        return True
+    return valid_utf8

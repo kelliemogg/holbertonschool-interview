a82f1b2cde328e41d50e728dd4ed11b4e92c3c6b
kelliemogg <kelliedmogg@gmail.com>
2021-10-05 09:46:34 -0800

    fixed 2048 game

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,96 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - reproduce the 2048 game
+ * description: slide and merge ints in an array
+ * and combine duplicates
+ * @line: array of integers
+ * @size: number of elements in line
+ * @direction: SLIDE_LEFT or SLIDE_RIGHT
+ * Return: 1 or 0
+ */
+
+int slide_line(int *line, size_t size, int direction)
+{
+    if (size <= 0)
+        return (0);
+    if (direction != SLIDE_RIGHT || direction != SLIDE_LEFT)
+        return (0);
+	if (direction == SLIDE_LEFT)
+	{
+		slide_left(line, size);
+	}
+	else if (direction == SLIDE_RIGHT)
+	{
+		slide_right(line, size);
+    }
+	return (1);
+}
+
+/**
+ * slide_left - reproduce the 2048 game
+ * description: slides to the left
+ * and combine duplicates, if any
+ * @line: array of integers
+ * @size: number of elements in line
+ * Return: 1 or 0
+ */
+
+int slide_left(int *line, size_t size)
+{
+    int x, check;
+
+	for (x = 0; x < size; x++) 
+    {
+		if (line[x] && line[x] == line[check])
+        {
+            line[x] += line[x];
+		}
+        else
+        {
+            if (line[check] != 0)
+            {
+                check++;
+            }
+            else
+            {
+                line[check] += line[x], line[x] = 0;
+            }
+        }
+	}
+    return (1);
+}
+
+/**
+ * slide_right - reproduce the 2048 game
+ * description: slides to the right
+ * and combine duplicates, if any
+ * @line: array of integers
+ * @size: number of elements in line
+ * Return: 1 or 0
+ */
+
+int slide_right(int *line, int size)
+{
+	int x, check;
+
+	for (x = size - 2; x >= 0; x--)
+	{
+		if (line[x] && line[x] == line[check])
+        {
+            line[check--] += line[x], line[x] = 0;
+        }
+		else
+        {
+            if (line[x] != 0)
+            {
+                check--;
+            }
+            else
+            {
+                line[check] += line[x], line[x] = 0;
+            }
+        }
+	}
+	return (1);
+}
---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef SLIDE_LINE_H
+#define SLIDE_LINE_H
+#define SLIDE_LEFT -1
+#define SLIDE_RIGHT 1
+#include <stdlib.h>
+#include <stdio.h>
+
+int slide_line(int *line, size_t size, int direction);
+int slide_right(int *line, int size);
+int slide_left(int *line, int size);
+
+#endif /* SLIDE_LINE_H */

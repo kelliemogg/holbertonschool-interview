3c06ff884cba55ad66538e2130c4c83734b7d1c6
kelliemogg <kelliedmogg@gmail.com>
2021-10-07 12:27:50 -0800

    fixed betty errors

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -3,20 +3,20 @@
 /**
 * menger - function to produce a 2D menger sponge
 * @level: level to be drawn
-* Return: 2D 
+* Return: 2D Image
 */
 
 void menger(int level)
 {
-    if (level < 0)
-        return;
-    if (level == 0)
-        printf("#\n");
-    if (level == 1)
-        printf("###\n# #\n###\n");
-    if (level == 2)
-    {
-        printf("#########\n# ## ## #\n#########\n###   ###\n");
-        printf("# #   # #\n###   ###\n#########\n# ## ## #\n#########\n");
-    }
+	if (level < 0)
+		return;
+	if (level == 0)
+		printf("#\n");
+	if (level == 1)
+		printf("###\n# #\n###\n");
+	if (level == 2)
+	{
+		printf("#########\n# ## ## #\n#########\n###   ###\n");
+		printf("# #   # #\n###   ###\n#########\n# ## ## #\n#########\n");
+	}
 }

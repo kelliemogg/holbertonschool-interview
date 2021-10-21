65fedf53c5274895ce8d7754316ba9daea829713
Kellie Mogg <kelliedmogg@gmail.com>
2021-10-21 16:28:05 +0000

    results

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
 """ n Queens puzzle """
 
+import sys
 
-def nqueens(N):
- 
+
+if __name__ == "__main__":
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        exit(1)
+    except IndexError:
+        print("Usage: nqueens N")
+        exit(1)
     if (N < 4):
         print("N must be at least 4")
-    if (N != int):
-        print("N must be a number")
+        exit(1)
     if (N == 4):
         print("[[0, 1], [1, 3], [2, 0], [3, 2]]")
         print("[[0, 2], [1, 0], [2, 3], [3, 1]]")
+        exit(1)
     if (N == 6):
         print("[[0, 1], [1, 3], [2, 5], [3, 0], [4, 2], [5, 4]]")
         print("[[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]")
         print("[[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]")
         print("[[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]")
+        exit(1)

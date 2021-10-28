e3c3a22a63f808942ffd6254ff47ef7b41031c7e
kelliemogg <kelliedmogg@gmail.com>
2021-10-27 16:10:20 -0800

    testing checker

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -26,7 +26,7 @@ typedef struct binary_tree_s avl_t;
 
 avl_t *sorted_array_to_avl(int *array, size_t size);
 void binary_tree_print(const binary_tree_t *tree);
-avl_t *binary_tree_node(avl_t *parent, int data);
-int find_middle(int start, int end);
+avl_t *binary_tree_insert(avl_t *root, int data);
+avl_t *binary_tree_node(avl_t *root, int first, int last, int *array);
 
 #endif

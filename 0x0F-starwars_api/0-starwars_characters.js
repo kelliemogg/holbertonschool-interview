1d052962a4ebdc49c849c4b82f3a45a606321e4a
kelliemogg <kelliedmogg@gmail.com>
2021-11-10 14:10:52 -0900

    .

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const args = process.argv.slice(2);
+console.log(printCharacters(args));
+
+function printCharacters(movieID) {
+  var url = `https://swapi.co/api/films/${args}/`;
+  request(url, function(error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      var data = JSON.parse(body);
+      var characters = data.characters;
+      for (var i = 0; i < characters.length; i++) {
+        var character = characters[i];
+        request(character, function(error, response, body) {
+          if (error) {
+            console.log(error);
+          } else {
+            var data = JSON.parse(body);
+            console.log(data.name);
+          }
+        });
+      }
+    }
+  });
+}

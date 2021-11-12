2b68e7a82c4a506bd4b10accb89b3ad96ad842ce
Kellie Mogg <kelliedmogg@gmail.com>
2021-11-12 01:55:15 +0000

    .

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,27 +2,28 @@
 
 const request = require('request');
 const args = process.argv.slice(2);
-console.log(printCharacters(args));
+const url = `https://swapi.co/api/films/${args}/`;
 
-function printCharacters(movieID) {
-  const url = `https://swapi.co/api/films/${args}/`;
-  request(url, function(error, response, body) {
+
+request(url, function (error, response, body) {
     if (error) {
-      console.log(error);
-    } else {
-        const data = JSON.parse(body);
-        const characters = data.characters;
-        for (let i = 0; i < characters.length; i++) {
-            const url = characters[i];
-            request(url, function(error, response, body) {
+        console.log(error);
+    }
+    const characters = JSON.parse(body).characters;
+
+    characters.forEach(function (character) {
+        request(character, function (error, response, body) {
             if (error) {
                 console.log(error);
-            } else {
-                const data = JSON.parse(body);
-                console.log(data.name);
             }
-            });
-        }
-        }
+            const charName = JSON.parse(body).name;
+            const listCharacters = {};
+            listCharacters[charName] = character;
+            if (Object.values(listCharacters).length === characters.length) {
+                characters.forEach(function (character) {
+                    console.log(listCharacters[character]);
+                });
+            }
+        });
     });
-}
+});

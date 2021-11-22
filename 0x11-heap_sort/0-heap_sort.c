91cb8a3891eba22df1cff7ba897bc6273c36839d
Kellie Mogg <kelliedmogg@gmail.com>
2021-11-22 23:03:21 +0000

    Sorting and printing after every iteration

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -11,52 +11,54 @@
  */
 void heap_sort(int *array, size_t size)
 {
-    size_t i;
-	int temp;
+    int i, temp;
 
-	for (i = size / 2 - 1; i > 0; i--)
-	{
-		sift_down(array, size, i);
-		print_array(array, size);
-	}
-
-	for (i = size - 1; i > 0; i--)
-	{
-		temp = array[0];
-		array[0] = array[i];
-		array[i] = temp;
-		sift_down(array, i, 0);
-		print_array(array, size);
-	}
+    for (i = size / 2 - 1; i >= 0; i--)
+    {
+        sift_down(array, i, size, size);
+    }
+    for (i = size - 1; i > 0; i--)
+    {
+        temp = array[0];
+        array[0] = array[i];
+        array[i] = temp;
+        if (i != 0)
+        {
+            print_array(array, size);
+        }
+        sift_down(array, 0, i, size);
+    }
 }
 
 /*
- * sift_down - sift down the element at index i and all elements in the heap
+ * sift_down - sift down the array to find the largest value
+ * description: sift down works on the path from the root to a leaf node
+ * at each state we need to compare the children to find which is larger,
+ * and then compare the larger child to the parent
  * @array: array to sort
+ * @i: index of the parent node
  * @size: size of array
- * @i: index to start sifting from
  */
-void sift_down(int *array, size_t size, size_t i)
+void sift_down(int *array, size_t i, size_t size, size_t temp_size)
 {
-	size_t left, right, largest;
-	int temp;
+    size_t left = 2 * i + 1;
+    size_t right = 2 * i + 2;
+    size_t largest = i;
 
-	left = 2 * i + 1;
-	right = 2 * i + 2;
-
-	if (left < size && array[left] > array[i])
-		largest = left;
-	else
-		largest = i;
-
-	if (right < size && array[right] > array[largest])
-		largest = right;
-
-	if (largest != i)
-	{
-		temp = array[i];
-		array[i] = array[largest];
-		array[largest] = temp;
-		sift_down(array, size, largest);
-	}
+    if (left < size && array[left] > array[largest])
+    {
+        largest = left;
+    }
+    if (right < size && array[right] > array[largest])
+    {
+        largest = right;
+    }
+    if (largest != i)
+    {
+        int temp = array[i];
+        array[i] = array[largest];
+        array[largest] = temp;
+        print_array(array, temp_size);
+        sift_down(array, largest, size, temp_size);
+    }
 }
--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -7,6 +7,6 @@
 
 void heap_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-void sift_down(int *array, size_t size, size_t i);
+void sift_down(int *array, size_t i, size_t size, size_t temp_size);
 
 #endif /* SORT_H */ 

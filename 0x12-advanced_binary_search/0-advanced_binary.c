404402e17fa99e256ac15a151a722c59a16abec4
Kellie Mogg <kelliedmogg@gmail.com>
2021-12-03 05:22:44 +0000

    null check for no value passed

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -11,22 +11,22 @@
 
 int advanced_binary(int *array, size_t size, int value)
 {
-    size_t start = 0, end = size - 1, mid;
+	size_t start = 0, end = size - 1, mid;
 
-    if (array == NULL)
-        return (-1);
-    if (!value)
-        return (-1);
+	if (array == NULL)
+		return (-1);
+	if (!value)
+		return (-1);
 
-    while (start <= end)
-    {
-        mid = (start + end) / 2;
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            start = mid + 1;
-        else
-            end = mid - 1;
-    }
-    return (-1);
+	while (start <= end)
+	{
+		mid = (start + end) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			start = mid + 1;
+		else
+			end = mid - 1;
+	}
+	return (-1);
 }

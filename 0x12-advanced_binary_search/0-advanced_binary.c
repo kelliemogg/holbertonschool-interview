e13501bd348fa49782fd2f49d7719d0212e8fbc5
Kellie Mogg <kelliedmogg@gmail.com>
2021-12-03 05:32:58 +0000

    Printing output in correct format

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -3,6 +3,7 @@
 /**
 * advanced_binary - search an array for a given value
 * description: takes in an array, the size, and a value to be found
+* prints the index of the value if found, or -1 if not found
 * @array: given array
 * @size: size of array
 * @value: value to search for
@@ -20,6 +21,7 @@ int advanced_binary(int *array, size_t size, int value)
 
 	while (start <= end)
 	{
+		print_array(array, start, end);
 		mid = (start + end) / 2;
 		if (array[mid] == value)
 			return (mid);
@@ -29,4 +31,16 @@ int advanced_binary(int *array, size_t size, int value)
 			end = mid - 1;
 	}
 	return (-1);
+}
+
+void print_array(int *array, size_t start, size_t end)
+{
+	printf("Searching in array: ");
+	for (; start <= end; start++)
+	{
+		printf("%d", array[start]);
+		if (start != end)
+			printf(", ");
+	}
+	printf("\n");
 }
--- 0x12-advanced_binary_search/search_algos.h
+++ 0x12-advanced_binary_search/search_algos.h
@@ -6,5 +6,6 @@
 #include <stdbool.h>
 
 int advanced_binary(int *array, size_t size, int value);
+void print_array(int *array, size_t start, size_t end);
 
 #endif /* SEARCH_ALGOS_H */

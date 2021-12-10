22fff5c259102e667ec8f93d1a608910bb1bf7f5
Kellie Mogg <kelliedmogg@gmail.com>
2021-12-10 04:46:25 +0000

    pep8

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -2,25 +2,29 @@
 """
 recursive function that queries the Reddit API, parses the title of all
 hot articles, and prints a sorted count of given keywords (case-insensitive,
-delimited by spaces. Javascript should count as javascript, but java should not.
+delimited by spaces. Javascript should count as javascript,
+but java should not.
 
 Prototype: def count_words(subreddit, word_list)
 if word_list contains the same word (case-insensitive), the final count should
 be the sum of each word's count.
 
-Results should be printed in descending order, by the count, and if the count is
-the same for separate keywords, they should be sorted alphabetically.
+Results should be printed in descending order, by the count,
+and if the count is the same for separate keywords, they
+should be sorted alphabetically.
 
 """
 import requests
 import json
 from collections import OrderedDict
 
+
 def count_words(subreddit, word_list, key_dict={}, count=0, after=None):
     """
     recursive function that queries the Reddit API, parses the title of all
-    hot articles, and prints a sorted count of given keywords (case-insensitive,
-    delimited by spaces. Javascript should count as javascript, but java should not.
+    hot articles, and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces. Javascript should count as
+    javascript, but java should not.
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     header = {'User-Agent': 'user'}

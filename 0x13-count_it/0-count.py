8a10ada4331406793344ac283640d47a3e6ed04b
Kellie Mogg <kelliedmogg@gmail.com>
2021-12-10 04:34:13 +0000

    correct count but still not in alphabetical order

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -16,7 +16,7 @@ import requests
 import json
 from collections import OrderedDict
 
-def count_words(subreddit, word_list, key_dict={}, after=None):
+def count_words(subreddit, word_list, key_dict={}, count=0, after=None):
     """
     recursive function that queries the Reddit API, parses the title of all
     hot articles, and prints a sorted count of given keywords (case-insensitive,
@@ -24,42 +24,42 @@ def count_words(subreddit, word_list, key_dict={}, after=None):
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     header = {'User-Agent': 'user'}
-    params = {"show:": "all", "next": "next", "after": after}
+    params = {"limit": 100, "after": after}
     key_dict = OrderedDict(key_dict)
     try:
         response = requests.get(url, headers=header, allow_redirects=False,
-                                params=params)
+                                params=params).json()
     except ValueError:
         return
     """ adding all keywords to a dictionary """
     """ get data from the response """
-    data = response.json()
-    """ parse the data and save to variables """
-    data_list = data.get('data')
-    after = data_list.get('after')
-    children = data_list.get('children')
+    data = response.get('data')
+    """ get the children """
+    children = data.get('children')
+    """ get the after """
+    after = data.get('after')
+
     """ loop through the children """
     for child in children:
         """ parse the child data """
-        child_data = child.get('data')
-        """ parse the child data and save to variables """
-        child_title = child_data.get('title')
-        child_title_lower = child_title.lower()
-        """ loop through the keywords """
-        for word in word_list:
-            """ if the keyword is in the child title """
-            if word in child_title_lower:
-                """ if the keyword is in the dictionary """
-                if word in key_dict:
-                    """ add one to the count """
-                    key_dict[word] += 1
-                else:
-                    """ add the keyword to the dictionary """
-                    key_dict[word] = 1
+        post = (child.get('data')['title']).lower()
+        """ split words in above request and loop through """
+        for p in post:
+            for word in word_list:
+                word = word.lower()
+                """ if the keyword is in the child title """
+                if p == word:
+                    """ if the keyword is in the dictionary """
+                    if word in key_dict.keys():
+                        """ add one to the count """
+                        key_dict[word] += 1
+                    else:
+                        """ add the keyword to the dictionary """
+                        key_dict[word] = 1
     """ if the after is not None """
     if after is not None:
         """ call the function again """
-        return count_words(subreddit, word_list, key_dict, after)
+        return count_words(subreddit, word_list, key_dict, count, after)
     else:
         """ sort the dictionary """
         for item in sorted(key_dict, key=key_dict.get, reverse=True):

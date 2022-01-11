c0564facb39275cb414821060c42929e65ac1f4d
Kellie Mogg <kelliedmogg@gmail.com>
2022-01-11 17:46:07 +0000

    Detecting a loop but returning the wrong node

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* find_listint_loop - finds the loop in a linked list
+* @head: pointer to the head of the list
+* Return: the node where the loop starts, or NULL if there is no loop
+*
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+    listint_t *slow = head;
+    listint_t *fast = head;
+
+    while (slow && fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return (slow);
+        }
+    }
+    return (NULL);
+}

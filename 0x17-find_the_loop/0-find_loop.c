401e7a1c63084289d690f04e6adde075cd4d245d
Kellie Mogg <kelliedmogg@gmail.com>
2022-01-12 18:14:50 +0000

    Upon finding a match, reset the slow node

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
 * find_listint_loop - finds the loop in a linked list
@@ -18,6 +19,12 @@ listint_t *find_listint_loop(listint_t *head)
 		fast = fast->next->next;
 		if (slow == fast)
 		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
 			return (slow);
 		}
 	}

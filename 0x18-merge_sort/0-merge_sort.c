651c80c4efa9d72c8f9b2688e4821b6a9ed3e62f
Kellie Mogg <kelliedmogg@gmail.com>
2022-01-17 20:57:28 +0000

    Sorting the first couple correctly

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,73 @@
+#include "sort.h"
+
+/**
+ * merge_sort - sorts an array of integers in ascending order using the
+ * Divide and Conquer algorithm
+ * @array: array of integers to sort
+ * @size: size of the array
+ * Return: void
+ */
+
+void merge_sort(int *array, size_t size)
+{
+    size_t middle;
+    int *left, *right;
+    int i = 0, j = 0;
+
+    if (size < 2)
+        return;
+
+    /* split the array in half, taking sure that the left is smaller than the right */
+    middle = size / 2;
+    left = malloc(sizeof(int) * size / 2);
+    right = malloc(sizeof(int) * size / 2 + 1);
+    /* add the left array to the left */
+    for (i = 0; (size_t)i < middle; i++)
+        left[i] = array[i];
+    /* add the right array to the right */
+    for (i = middle, j = 0; (size_t)i < size; i++, j++)
+        right[j] = array[i];
+    
+    merge_sort(left, middle);
+    merge_sort(right, size - middle);
+    merge(left, right, size);
+}
+
+/**
+ * merge - merges two sorted arrays into one sorted array
+ * @left: array of integers to sort
+ * @right: array of integers to sort
+ * @size: size of the array
+ * Return: new array
+ * 
+ */
+
+void merge(int *left, int *right, size_t size)
+{
+    int *new_array;
+    new_array = malloc(sizeof(int) * size);
+
+
+    /* merge the two arrays */
+    for (int i = 0, j = 0, k = 0; (size_t)i < size; i++)
+    {
+        if ((size_t)j < size / 2 && (size_t)k < size / 2)
+        {
+            if (left[j] < right[k])
+                new_array[i] = left[j++];
+            else
+                new_array[i] = right[k++];
+        }
+        else if ((size_t)j < size / 2)
+            new_array[i] = left[j++];
+        else
+            new_array[i] = right[k++];
+    }
+    printf("Merging...\n");
+    printf("[left]: ");
+    print_array(left, size / 2);
+    printf("[right]: ");
+    print_array(right, size / 2);
+    printf("[Done]: ");
+    print_array(new_array, size);
+}
---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void merge(int *left, int *right, size_t size);
+
+#endif

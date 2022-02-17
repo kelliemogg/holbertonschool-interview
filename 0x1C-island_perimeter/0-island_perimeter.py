9116d4213e34e91d4f7c7d2ddb5cf08bef0952e8
kelliemogg <kelliedmogg@gmail.com>
2022-02-17 14:35:58 -0600

    found perimeter of island

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,23 +1,25 @@
-class Sol:
-    def island_perimeter(self,lol):
-        res=0
-        lol.append([0]*len(lol[0]))
-        for i in range(len(lol)):
-            lol[i].append(0)
-        for i in range(len(lol)):
-            for j in range(len(lol[0])):
-                if lol[i][j]==1:
-                    res+=4
-                    if i+1 and lol[i+1][j]==1:
-                        res-=2
-                    if lol[i][j+1]==1:
-                        res-=2
-        return res
+#!/usr/bin/python3
 
-if __name__ == '__main__':
-    p1=Sol()
-    print(p1.island_perimeter([[0,1,0,0],
-                               [1,1,1,0],
-                               [0,1,0,0],
-                               [1,1,0,0]]))
-                               
+""" Func to calculate the perimeter of an 'island' made of up a list of
+    int that form rows and columns """
+
+def island_perimeter(grid):
+    """ Func to calculate the perimeter of an 'island' made of up a list of
+        int that form rows and columns """
+    perimeter = 0
+    m = len(grid)
+    n = len(grid[0])
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 1:
+                # Check if the cell is on the edge
+                if i == 0 or j == 0:
+                    perimeter += 1
+                if i == m - 1 or j == n - 1:
+                    perimeter += 1
+                # Check if the cell is on the edge
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter += 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter += 1
+    return perimeter

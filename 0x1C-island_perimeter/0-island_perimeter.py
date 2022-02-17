a533c975f69dc8d20c84f7b7379b17be7f211f97
kelliemogg <kelliedmogg@gmail.com>
2022-02-17 14:44:58 -0600

    added conditionals to account for every side

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
 
 """ Func to calculate the perimeter of an 'island' made of up a list of
-    int that form rows and columns """
+	int that form rows and columns """
 
 def island_perimeter(grid):
-    """ Func to calculate the perimeter of an 'island' made of up a list of
-        int that form rows and columns """
-    perimeter = 0
-    m = len(grid)
-    n = len(grid[0])
-    for i in range(m):
-        for j in range(n):
-            if grid[i][j] == 1:
-                # Check if the cell is on the edge
-                if i == 0 or j == 0:
-                    perimeter += 1
-                if i == m - 1 or j == n - 1:
-                    perimeter += 1
-                # Check if the cell is on the edge
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter += 1
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter += 1
-    return perimeter
+	""" Func to calculate the perimeter of an 'island' made of up a list of
+		int that form rows and columns """
+	perimeter = 0
+	m = len(grid)
+	n = len(grid[0])
+	for i in range(m):
+		for j in range(n):
+			if grid[i][j] == 1:
+				left = j - 1
+				right = j + 1
+				top = i - 1
+				bottom = i + 1
+
+				if left < 0 or grid[i][left] == 0:
+					perimeter += 1
+				if right >= n or grid[i][right] == 0:
+					perimeter += 1
+				if top < 0 or grid[top][j] == 0:
+					perimeter += 1
+				if bottom >= m or grid[bottom][j] == 0:
+					perimeter += 1
+					
+	return perimeter

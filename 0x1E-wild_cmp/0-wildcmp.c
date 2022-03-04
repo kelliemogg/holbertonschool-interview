7b92400de2c57427e8e6b6ff00f2997f02baedb6
kelliemogg <kelliedmogg@gmail.com>
2022-03-04 10:42:27 -0600

    Using for loops to compare chars in s1 and s2

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -11,10 +11,21 @@
 
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-        return (1);
-    /* if *s2 is *, then it can match an empty string */
-    if (*s2 == '*' && *(s2 + 1) == '\0' && *s1 == '\0')
-        return (1);
-    return (0);
+	int i, j;
+	
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	/* if *s2 is *, then it can match an empty string */
+	if (*s2 == '*' && *(s2 + 1) == '\0' && *s1 == '\0')
+		return (1);
+	/* loop through s1 and s2 */
+	for (i = 0; i < s1[i]; i++)
+	{
+		for (j = 0; j < s2[j]; j++)
+		{
+			if (s1[i] == s2[j] || s2[j] == '*')
+				return (wildcmp(s1 + 1, s2 + 1));
+		}
+	}
+	return (0);
 }

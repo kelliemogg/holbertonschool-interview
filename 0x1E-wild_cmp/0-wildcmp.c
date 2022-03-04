0c2c1acaf4bb32cb0dd241e0377a4aa9581370ae
kelliemogg <kelliedmogg@gmail.com>
2022-03-04 11:14:59 -0600

    Removed prohibitive check

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -13,9 +13,6 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	/* if *s2 is *, then it can match an empty string */
-	if (*s1 == '\0' && *s2 != '\0')
-		return (0);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
